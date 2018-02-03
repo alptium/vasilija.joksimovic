62b2cb77ecd58576d6dede1f51fb2fd7ddcc0f1f
vasilijajoksimovic <35069869+vasilijajoksimovic@users.noreply.github.com>
2018-02-03 14:08:53 +0100

    Updated

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("My name is Vasilija");
+
+	}
+
+}
