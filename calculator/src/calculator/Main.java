60e92e27c4ab7c9961f14c5755535d03df322e01
vasilijajoksimovic <35069869+vasilijajoksimovic@users.noreply.github.com>
2018-02-06 23:55:29 +0100

    Upload

---
+++ calculator/src/calculator/Main.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try(Scanner sc = new Scanner(System.in)) {
+			
+			System.out.println("Enter your first number");
+			double firstNum = sc.nextDouble();
+			
+			System.out.println("Choose the operation ==>  +, -, * ,/ ");
+			String operation = sc.next();
+			
+			System.out.println("Enter your second number");
+			double secondNum = sc.nextDouble();
+
+			System.out.println("Now the system is calculating the result ...");
+			double result = 0.0;
+			
+			if(operation.equals("+")) {
+				result = firstNum + secondNum;
+			} else if(operation.equals("-")) {
+				result = firstNum - secondNum;
+			} else if(operation.equals("*")) {
+				result = firstNum * secondNum;		
+			} else if(operation.equals("/")) {
+				result = firstNum / secondNum;			
+			}
+			
+			System.out.println("First number is: " + firstNum);
+			System.out.println("Second number is: " + secondNum);
+			System.out.println("The result is: " + result);
+
+		}
+
+	}
+	
+}	
