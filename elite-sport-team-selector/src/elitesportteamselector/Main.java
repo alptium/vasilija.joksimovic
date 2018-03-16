16f3a0257c453b83bfdb3e804fdc7bd15eaf5392
Valentina Cupac <valentina.cupac@optivem.com>
2018-03-16 21:58:52 +0100

    #6 Code Review Fixes

--- elite-sport-team-selector/src/elitesportteamselector/Main.java
+++ elite-sport-team-selector/src/elitesportteamselector/Main.java
@@ -4,19 +4,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args)  {
+	
+	public static void main(String[] args) {
 
 		System.out.println("Welcome to the application for MMA player registration");
 		System.out.println("Please, follow the instructions and fill in the application...");
 		System.out.println();
 		
-		ArrayList<Candidate> acceptedCandidateList = new ArrayList<>();
-		ArrayList<Candidate> rejectedCandidateList = new ArrayList<>();
-
+		ArrayList<Candidate> acceptedCandidates = new ArrayList<>();
+		ArrayList<Candidate> rejectedCandidates = new ArrayList<>();
 		
-		try(Scanner sc = new Scanner(System.in)){
+		try(Scanner sc = new Scanner(System.in)) {
 			
-			while(true){
+			while(true) {
 			
 				System.out.println("Enter candidate's name");
 				String name = sc.next();
@@ -45,28 +45,25 @@ public class Main {
 					System.out.println("The candidate does not meet the requirements of this preselection");
 					System.out.println("The application is REJECTED");
 					System.out.println();	
-					rejectedCandidateList.add(candidate);
+					rejectedCandidates.add(candidate);
 				} else {
 					System.out.println("The candidate meets the requirements of this preselection");
 					System.out.println("The application is ACCEPTED");
 					System.out.println();
-					acceptedCandidateList.add(candidate);
-
-			}	
+					acceptedCandidates.add(candidate);
+				}	
 			
 				System.out.println("Do you want to enter the data for a new candidate? yes/no");
 				System.out.println();
 			
 				String quit = sc.next(); 
 			
-				if (quit.equals("no")) {
-					break;
-				} else while (!quit.equals("no") && !quit.equals("yes")) {
+				while (!quit.equals("no") && !quit.equals("yes")) {
 					System.out.println("You entered the wrong word, plase response with yes or no!");
 					System.out.println("Do you want to enter the data for a new candidate? yes/no");
 					System.out.println();
 					
-					quit = sc.next();
+					quit = sc.next();						
 				}
 			
 				if (quit.equals("no")) {
@@ -75,37 +72,37 @@ public class Main {
 					
 			}
 			
-			int i = 1;
-			
 			System.out.println("List of accepted candidates:");
 			System.out.println();
 			
-			for(Candidate candidate : acceptedCandidateList) {
-				
+			int i = 1;
+			
+			for(Candidate candidate : acceptedCandidates) {
 				System.out.println("" + i + "." + "\t" + candidate.getName() + "\t" + candidate.getSurname());
 				i++;
 			}
-		
-			int j = 1;
+
 			System.out.println("List of rejected candidates:");
 			System.out.println();
-		
-			for(Candidate candidate : rejectedCandidateList) {
+
+			int j = 1;
 			
+			for(Candidate candidate : rejectedCandidates) {
 				System.out.println("" + j + "." + "\t" + candidate.getName() + "\t" + candidate.getSurname());
 				j++;
 			}
 		
-			int totalNumOfPeople = (i + j) - 2 ;
+			int acceptedCandidatesCount = acceptedCandidates.size();
+			int rejectedCandidatesCount = rejectedCandidates.size();
+			int totalCandidatesCount = acceptedCandidatesCount + rejectedCandidatesCount;
+			
+			double acceptedCandidatesPercent = (double)acceptedCandidatesCount * 100 / totalCandidatesCount;
+			double rejectedCandidatesPercent = (double)rejectedCandidatesCount * 100 / totalCandidatesCount;
 
 			System.out.println();
-			System.out.println("Total number of applied candidates is: " + totalNumOfPeople);
-			System.out.println("Total number of ccandiidates that were accepted is " + (i - 1) + ", that makes" + "\t" + ((i - 1) * 100 / totalNumOfPeople) + "%");
-			System.out.println("Total number of candidates that were rejected is " + (j - 1) + ", that makes" + "\t" + ((j - 1) * 100 / totalNumOfPeople) + "%");
-			
-			}
-		
-		
+			System.out.println("Total number of applied candidates is: " + totalCandidatesCount);
+			System.out.println("Total number of candidates that were accepted is " + acceptedCandidatesCount + ", that makes" + "\t" + acceptedCandidatesPercent + "%");
+			System.out.println("Total number of candidates that were rejected is " + rejectedCandidatesCount + ", that makes" + "\t" + rejectedCandidatesPercent + "%");
 		}
-	
+	}
 }
